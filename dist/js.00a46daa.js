16756144c99766acd3183ea0463c378ff009e41a
Jakub Figlak <jakub.figlak@gmail.com>
2020-03-07 15:17:01 +0100

    Build project for production

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -21327,7 +21327,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51874" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61624" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
